40975a22c763015b9638245f3915fd8d4a0422c5
Rafael Silva <rafaaelsilva21@hotmail.com>
2024-11-06 19:32:18 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+</head>
+<body>
+<?php
+
+echo 'Olá, mundo';
+?>
+
+<div class="button-container">
+    <button onclick="window.location.href='adicionar_aluno.php';">Adicionar Alunos</button>
+    <button onclick="window.location.href='listar_alunos.php';">Listar Alunos</button>
+</div>
+
+</body>
+</html>
