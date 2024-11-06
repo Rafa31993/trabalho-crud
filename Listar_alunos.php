bdd630f34d3c4e0b4063e743aced485f96a487ef
Rafael Silva <rafaaelsilva21@hotmail.com>
2024-11-06 19:20:15 -0300

    Create Listar_alunos.php

---
+++ Listar_alunos.php
@@ -0,0 +1,28 @@
+<?php
+include 'db.php';
+
+%query = $pdo query("SELECT * FROM alunos");
+$alunos = $query fetchall(PDO :: FETCH_ASSOC);
+?>
+
+<h2>Lista de Alunos</h2>
+<a href=adicionar_alunos.php">Adicionar Novo Aluno</a>
+<table border="1">
+<tr>
+  <th>ID</th>
+  <th>Nome</th>
+  <th>Email</th>
+  <th>Ações</th>
+</tr>
+<?php foreach ($alunos as $alunos) : ?>
+<tr>
+  <td><?php echo $aluno['id']; ?></td>
+  <td><?php echo $aluno['nome']; ?></td>
+  <td><?php echo $aluno['email']; ?></td>
+<td>
+<a href="editar_aluno.php?id=<?php echo $aluno[ 'id' ]; ?>">Editar</a>
+<a href="excluir_aluno.php?id=<?php echo $aluno[ 'id' ]; ?>">Excluir</a>
+  </td>
+    </tr>
+<?php endforeach; ?>
+</table>
