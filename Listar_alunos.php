65ce7527b1e9ba8bcd06f3a114e80f40898bbb6b
Rafael Silva <rafaaelsilva21@hotmail.com>
2024-11-06 19:31:37 -0300

    Update Listar_alunos.php

--- Listar_alunos.php
+++ Listar_alunos.php
@@ -1,28 +1,38 @@
 <?php
-include 'db.php';
+// Incluir a classe Produto
+//include 'db.php';
 
-%query = $pdo query("SELECT * FROM alunos");
-$alunos = $query fetchall(PDO :: FETCH_ASSOC);
+// $query = $pdo -> query("SELECT * FROM aluno");
+//$alunos = $query -> fetchALL(PDO::FETCH_ASSOC);
+
+require_once 'db.php';
+
+$aluno = new Aluno();
+
+$alunos = $aluno->ListarAlunos();
+
 ?>
+    <h2>Lista de Alunos</h2>
 
-<h2>Lista de Alunos</h2>
-<a href=adicionar_alunos.php">Adicionar Novo Aluno</a>
-<table border="1">
-<tr>
-  <th>ID</th>
-  <th>Nome</th>
-  <th>Email</th>
-  <th>Ações</th>
-</tr>
-<?php foreach ($alunos as $alunos) : ?>
-<tr>
-  <td><?php echo $aluno['id']; ?></td>
-  <td><?php echo $aluno['nome']; ?></td>
-  <td><?php echo $aluno['email']; ?></td>
-<td>
-<a href="editar_aluno.php?id=<?php echo $aluno[ 'id' ]; ?>">Editar</a>
-<a href="excluir_aluno.php?id=<?php echo $aluno[ 'id' ]; ?>">Excluir</a>
-  </td>
-    </tr>
-<?php endforeach; ?>
-</table>
+    <a href="adicioar_aluno.php">Adicionar Novo Aluno</a>
+        <table border="1">
+            
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Ações</th>                    
+                </tr>
+           
+                <?php foreach ($alunos as $aluno): ?>
+                    <tr>
+                        <td><?php echo $aluno['id']; ?></td>
+                        <td><?php echo $aluno['nome']; ?></td>
+                        <td><?php echo $aluno['email']; ?></td>
+                        <td>
+                            <a href="editar_aluno.php?id=<?php echo $aluno['id']; ?>">Editar</a>
+                            <a href="excluir_aluno.php?id=<?php echo $aluno['id']; ?>">Excluir</a>
+                    </tr>
+                <?php endforeach; ?>
+            
+        </table>
